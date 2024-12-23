5b6032570dda57b7faf92ecafad3ae31411da287
gersondantas <53773325+GersonDantas@users.noreply.github.com>
2024-12-23 19:04:30 -0300

    feat(Transactions): add transaction table and styling

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,38 @@
 import { Header } from "../../components/Header";
 import { Summary } from '../../components/Summary';
+import { PriceHighlight } from './styles';
+import { TransactionsContainer } from './styles';
 
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+
+      <TransactionsContainer>
+        <table>
+          <tbody>
+            <tr>
+              <td>Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+              </td>
+            </tr>
+            <tr>
+              <td>Aluguel</td>
+              <td>
+                <PriceHighlight variant="outcome">- R$ 1.100,00</PriceHighlight>
+              </td>
+            </tr>
+            <tr>
+              <td>carro</td>
+              <td>
+                <PriceHighlight variant="income">R$ 50.000,00</PriceHighlight>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </TransactionsContainer>
     </div>
   )
 }
