a224e85ac11a1c243e5b0549aa3ae4120a674c38
gersondantas <53773325+GersonDantas@users.noreply.github.com>
2024-12-23 19:04:45 -0300

    feat(transactions): add SearchForm component and update TransactionsTable

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,8 +1,8 @@
 import { Header } from "../../components/Header";
 import { Summary } from '../../components/Summary';
-import { PriceHighlight } from './styles';
+import { PriceHighlight, TransactionsTable } from './styles';
 import { TransactionsContainer } from './styles';
-
+import { SearchForm } from './components/SearchForm';
 export function Transactions() {
   return (
     <div>
@@ -10,28 +10,35 @@ export function Transactions() {
       <Summary />
 
       <TransactionsContainer>
-        <table>
+        <SearchForm />
+        <TransactionsTable>
           <tbody>
             <tr>
-              <td>Desenvolvimento de site</td>
+              <td width="50%">Desenvolvimento de site</td>
               <td>
                 <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
               </td>
+              <td>Venda</td>
+              <td>13/12/2022</td>
             </tr>
             <tr>
-              <td>Aluguel</td>
+              <td width="50%">Aluguel</td>
               <td>
                 <PriceHighlight variant="outcome">- R$ 1.100,00</PriceHighlight>
               </td>
+              <td>Aluguel</td>
+              <td>13/12/2022</td>
             </tr>
             <tr>
-              <td>carro</td>
+              <td width="50%">carro</td>
               <td>
                 <PriceHighlight variant="income">R$ 50.000,00</PriceHighlight>
               </td>
+              <td>carro</td>
+              <td>13/12/2022</td>
             </tr>
           </tbody>
-        </table>
+        </TransactionsTable>
       </TransactionsContainer>
     </div>
   )
