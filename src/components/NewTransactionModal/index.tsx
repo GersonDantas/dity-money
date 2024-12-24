7d6d70bb4bbfafb67d62be840fc965485718f40c
gersondantas <53773325+GersonDantas@users.noreply.github.com>
2024-12-24 08:17:29 -0300

    feat(NewTransactionModal): add transaction type selection UI

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,6 @@
 import * as Dialog from "@radix-ui/react-dialog";
-import { CloseButton, Content, Overlay } from './styles';
-import { X } from '@phosphor-icons/react';
+import { CloseButton, Content, Overlay, TransactionType, TransactionTypeButton } from './styles';
+import { ArrowDown, ArrowUp, X } from '@phosphor-icons/react';
 
 export function NewTransactionModal () {
   return (
@@ -18,6 +18,18 @@ export function NewTransactionModal () {
           <input type="text" placeholder="Descrição" required />
           <input type="number" placeholder="Preço" required />
           <input type="text" placeholder="Categoria" required />
+
+          <TransactionType>
+            <TransactionTypeButton variant="income">
+              <ArrowUp />
+              Entrada
+            </TransactionTypeButton>
+
+            <TransactionTypeButton variant="outcome">
+              <ArrowDown />
+              Saída
+            </TransactionTypeButton>
+          </TransactionType>
 
           <button type="submit">Cadastrar</button>
         </form>
